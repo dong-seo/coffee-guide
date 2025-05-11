b9644f8b64e6bef422637cd71d409768ffcd6344
DongJun <dongjun8983@gmail.com>
2025-05-11 18:31:38 +0900

    공부를 위한 프로젝트 생성.

---
+++ src/main/java/org/example/coffeeguide/CoffeeGuideApplication.java
@@ -0,0 +1,13 @@
+package org.example.coffeeguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/coffeeguide/CoffeeGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.coffeeguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
